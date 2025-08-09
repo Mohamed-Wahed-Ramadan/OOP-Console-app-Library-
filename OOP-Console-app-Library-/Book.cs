38fda39110f12dc17d1ba69120ee8930793a775b
Mohamed Sharqawy <msharqawy21@gmail.com>
2025-08-09 19:34:55 +0300

    Update Book.cs
    this is Book Class with a default that requires all arguments

--- OOP-Console-app-Library-/Book.cs
+++ OOP-Console-app-Library-/Book.cs
@@ -8,5 +8,59 @@ namespace OOP_Console_app_Library_
 {
     internal class Book
     {
+         public string ID;
+ public string Title;
+ public string Author;
+ private bool Availability;
+ 
+
+ 
+
+ public Book(string id, string title, string author)
+ {
+     if(ValidateID(id))
+         ID = id;
+
+     if(ValidateAuthor(author))
+        Author = author;
+
+     if (ValidateTitle(title))
+        Title = title;
+     
+ }
+
+ public bool ValidateTitle(string title)
+ {
+     if(string.IsNullOrWhiteSpace(title))
+     {
+         Console.WriteLine("Can't Create A Book Without a Title");
+         return false;
+     }
+     return true;
+ }
+ public bool ValidateAuthor(string author)
+ {
+     if (string.IsNullOrWhiteSpace(author))
+     {
+         Console.WriteLine("Can't Create A Book Without a Author");
+         return false;
+     }
+     return true;
+ }
+ public bool ValidateID(string ID)
+ {
+     if (string.IsNullOrWhiteSpace(ID) && (ID.Length == 10 || ID.Length == 13))
+     {
+         for (int i  = 0;  i < ID.Length; i++)
+         {
+             if (!char.IsDigit(ID[i]))
+             {
+                 Console.WriteLine("ID has to be only Numbers");
+             }
+         }
+         return true;
+     }
+     return false;
+ }
     }
 }
