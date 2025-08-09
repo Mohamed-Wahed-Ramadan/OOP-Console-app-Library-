fe3623131fc433fca67c6d00ebf20d04a583d115
Mohamed-Wahed <medo03459@gmail.com>
2025-08-09 18:55:14 +0300

    The project's files
    files of the library console app

---
+++ OOP-Console-app-Library-/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Console_app_Library_
+{
+    internal class Book
+    {
+    }
+}
