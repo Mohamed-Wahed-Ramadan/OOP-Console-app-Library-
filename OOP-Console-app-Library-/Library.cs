5443785518a764e3d3f1909c239d132ad483e764
Mohamed Sharqawy <msharqawy21@gmail.com>
2025-08-09 20:33:35 +0300

    Update Library.cs
    The Add , Remove and List Books Part

--- OOP-Console-app-Library-/Library.cs
+++ OOP-Console-app-Library-/Library.cs
@@ -8,5 +8,40 @@ namespace OOP_Console_app_Library_
 {
     internal class Library
     {
+        public List<Book> Books;
+
+
+
+public void AddBook(Book book)
+{
+    for(int i = 0; i < Books.Count; i++)
+    {
+        if(Books[i].Title == book.Title)
+        {
+            Console.WriteLine($"The book with the name {book.Title} is already exist");
+        }
+        Books.Add(book);
+    }
+}
+
+public void RemoveBook(Book book)
+{
+    for (int i = 0;i < Books.Count;i++)
+    {
+        if (Books[i].Title == book.Title)
+        {
+            Books.Remove(Books[i]);
+        }
+        Console.WriteLine($"The Book with the name {book.Title} Maybe removed already");
+    }
+}
+
+public void GetBooks()
+{
+    foreach (Book book in Books)
+    {
+        Console.WriteLine($"Name {book.Title}, and the Author is {book.Author} With The ISBN of {book.ID}");
+    }
+}
     }
 }
