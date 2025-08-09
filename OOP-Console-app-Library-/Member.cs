a8b0ee7356ed5fc449b2287d928e14f663de9d87
amr essam fouad <a.e.fouad2014@gmail.com>
2025-08-09 19:58:22 +0300

    Update Member.cs

--- OOP-Console-app-Library-/Member.cs
+++ OOP-Console-app-Library-/Member.cs
@@ -8,5 +8,49 @@ namespace OOP_Console_app_Library_
 {
     internal class Member
     {
+         private int Id;
+ private static int IDcount = 0;
+ private string Name;
+ //public List<string> Names;
+ //Names.remove(Name[i]);
+
+ private Book[] _borrowedBooks;
+
+ public Member(string name)
+ {
+     if (string.IsNullOrWhiteSpace(name))
+     {
+         Console.WriteLine("Name cannot be empty.");
+     }
+     else
+     {
+         Name = name;
+     }
+     Id = IDcount++;
+     _borrowedBooks = 0;
+ }
+
+ public void removeMember(Member _Member)
+ {
+     for (int i = 0; i < IDcount; i++)
+     {
+         if (_borrowedBooks == 0 && _Member.Id == Id)
+         {
+             Console.WriteLine("Member found."); 
+         }
+         else
+         {
+             Console.WriteLine("Cannot remove member with borrowed books.");
+         }
+     }
+
+ }
+ public void DisplayMember()
+ {
+     for (int i = 0; i < IDcount; i++)
+     {
+         Console.WriteLine($"Member Name:{Name} and his Id: {Id}");
+     }
+ }
     }
 }
