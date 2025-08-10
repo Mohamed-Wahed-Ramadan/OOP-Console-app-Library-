0480cb5aa4654245e5e96f75fcdc4c243e4047d9
Mohamed-Wahed <medo03459@gmail.com>
2025-08-10 21:36:28 +0300

    New update functions
    update functions borrow book and function return book

--- OOP-Console-app-Library-/Library.cs
+++ OOP-Console-app-Library-/Library.cs
@@ -82,17 +82,18 @@ public void RemoveBook(Book book)
 
 public void GetBooks()
 {
-    if(Book.Count == 0)
-    {
-        Console.WriteLine("There is No Book at the moment!")
-        }
-    else{
-            foreach (Book book in Books)
+            if (Book.Count == 0)
             {
-                Console.WriteLine($"Name {book.Title}, and the Author is {book.Author} With The ISBN of {book.ID}");
+                Console.WriteLine("There is No Book at the moment!")
+                }
+            else
+            {
+                foreach (Book book in Books)
+                {
+                    Console.WriteLine($"Name {book.Title}, and the Author is {book.Author} With The ISBN of {book.ID}");
+                }
             }
         }
-}
 
          public void Display_available_borrowed_books()
  {
